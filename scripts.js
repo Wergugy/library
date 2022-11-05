083c211cedb6b8147e1cb23e48f3978b7b6825b4
Wergugy <wergugy@gmail.com>
2022-11-04 20:59:27 -0700

    Got the addBook process down
    Now properly creates objects based on the form
    and adds them to the library array. Just need to
    create a function that creates and populates a card.
    Will first do the populate portion then work on the
    card creation.

--- scripts.js
+++ scripts.js
@@ -5,20 +5,23 @@ function book(title, author, genre, pages, read) {
         this.author = author
         this.genre = genre
         this.pages = pages
-        this.read = read
+        this.read = read ? 'Read' : 'Unread';
 };
 
-function addNewBook() {
-        const bookInfo = document.querySelectorAll('#form');
+function addNewBook(event) {
+        event.preventDefault();
 
-        const title = bookInfo.filter((info) => info.classList.contains('title')).value;
-        const author = bookInfo.filter((info) => info.classList.contains('author')).value;
-        const genre = bookInfo.filter((info) => info.classList.contains('genre')).value;
-        const pages = bookInfo.filter((info) => info.classList.contains('pages')).value;
-        const read = bookInfo.filter((info) => info.classList.contains('read')).value;
+        const bookInfo = Array.from(document.querySelectorAll('.info'));
+
+        const title = bookInfo.find(info => info.classList.contains('title')).value;
+        const author = bookInfo.find(info => info.classList.contains('author')).value;
+        const genre = bookInfo.find(info => info.classList.contains('genre')).value;
+        const pages = bookInfo.find(info => info.classList.contains('pages')).value;
+        const read = bookInfo.find(info => info.classList.contains('read')).checked;
 
         const bookToAdd = new book(title, author, genre, pages, read);
 
         library.push(bookToAdd);
 };
 
+document.querySelector('.addBook').addEventListener('click', addNewBook);
