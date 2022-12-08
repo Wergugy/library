3772bf948b7b451c2cc0cb59b8eae871a4a7bd25
Wergugy <wergugy@gmail.com>
2022-12-07 23:56:32 -0800

    Start class reformat

--- scripts.js
+++ scripts.js
@@ -1,58 +1,60 @@
 const library = [];
 
-function book(title, author, genre, pages, read) {
-        this.title = title ? title : 'N/A'
-        this.author = author ? author : 'N/A'
-        this.genre = genre ? genre : 'N/A'
-        this.pages = pages ? pages : 'N/A'
-        this.read = read ? 'Read' : 'Unread';
+class book {
+        constructor(title, author, genre, pages, read) {
+                this.title = title ? title : 'N/A';
+                this.author = author ? author : 'N/A';
+                this.genre = genre ? genre : 'N/A';
+                this.pages = pages ? pages : 'N/A';
+                this.read = read ? 'Read' : 'Unread';
+        }
+        createCard() {
+                const newCard = document.createElement('div');
+                newCard.classList.add('card');
+                newCard.setAttribute('data-card', `${library.length}`);
+
+                const title = document.createElement('p');
+                title.classList.add('title');
+                title.textContent = this.title;
+                newCard.appendChild(title);
+
+                const author = document.createElement('p');
+                author.classList.add('author');
+                author.textContent = this.author;
+                newCard.appendChild(author);
+
+                const genre = document.createElement('p');
+                genre.classList.add('genre');
+                genre.textContent = this.genre;
+                newCard.appendChild(genre);
+
+                const pages = document.createElement('p');
+                pages.classList.add('pages');
+                pages.textContent = this.pages;
+                newCard.appendChild(pages);
+
+                const read = document.createElement('p');
+                read.classList.add('read');
+                read.textContent = this.read;
+                newCard.appendChild(read);
+
+                const readButton = document.createElement('button');
+                readButton.classList.add('readButton');
+                readButton.textContent = (this.read === 'Read') ? 'Unread' : 'Read';
+                newCard.appendChild(readButton);
+
+                const removeButton = document.createElement('button');
+                removeButton.classList.add('removeButton');
+                removeButton.textContent = 'Remove';
+                newCard.appendChild(removeButton);
+
+                document.querySelector('.carousel').appendChild(newCard);
+
+                document.querySelectorAll('.removeButton').forEach(b => b.addEventListener('click', removeBook));
+                document.querySelectorAll('.readButton').forEach(b => b.addEventListener('click', readUnread));
+        }
 };
 
-book.prototype.createCard = function() {
-        const newCard = document.createElement('div');
-        newCard.classList.add('card');
-        newCard.setAttribute('data-card',`${library.length}`);
-
-        const title = document.createElement('p');
-        title.classList.add('title');
-        title.textContent = this.title;
-        newCard.appendChild(title);
-
-        const author = document.createElement('p');
-        author.classList.add('author');
-        author.textContent = this.author;
-        newCard.appendChild(author);
-
-        const genre = document.createElement('p');
-        genre.classList.add('genre');
-        genre.textContent = this.genre;
-        newCard.appendChild(genre);
-
-        const pages = document.createElement('p');
-        pages.classList.add('pages');
-        pages.textContent = this.pages;
-        newCard.appendChild(pages);
-
-        const read = document.createElement('p');
-        read.classList.add('read');
-        read.textContent = this.read;
-        newCard.appendChild(read);
-
-        const readButton = document.createElement('button');
-        readButton.classList.add('readButton');
-        readButton.textContent = (this.read === 'Read') ? 'Unread' : 'Read';
-        newCard.appendChild(readButton);
-
-        const removeButton = document.createElement('button');
-        removeButton.classList.add('removeButton');
-        removeButton.textContent = 'Remove';
-        newCard.appendChild(removeButton);
-
-        document.querySelector('.carousel').appendChild(newCard);
-
-        document.querySelectorAll('.removeButton').forEach(b => b.addEventListener('click', removeBook));
-        document.querySelectorAll('.readButton').forEach(b => b.addEventListener('click', readUnread));
-};
 
 function addNewBook(event) {
         event.preventDefault();
